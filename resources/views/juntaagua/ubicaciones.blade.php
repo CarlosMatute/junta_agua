bdf5df6cfa486dd9e8ecdefeb9f73e87db93305e
Jose Zavala <jmariozavala28@gmail.com>
2024-05-20 21:36:14 -0600

    Correccion de iconos al guardar

--- resources/views/juntaagua/ubicaciones.blade.php
+++ resources/views/juntaagua/ubicaciones.blade.php
@@ -741,7 +741,7 @@
                                 }
 
                                 if (row.cliente_habita) {
-                                    cliente_habita_icono = '<i data-lucide="Check" class="w-4 h-4 text-green-500" style="color: #10B981;"></i>'
+                                    cliente_habita_icono = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="Check" data-lucide="Check" class="lucide lucide-Check w-4 h-4 text-green-500" style="color: #10B981;"><polyline points="20 6 9 17 4 12"></polyline></svg>';
                                     //cliente_habita_icono = 'SI';
                                 }else{
                                     cliente_habita_icono = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="X" data-lucide="X" class="lucide lucide-X w-4 h-4 text-red-500" style="color: #EF4444;"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>';
@@ -749,18 +749,18 @@
                                 }
 
                                 if (row.activo) {
-                                     activo_icono = '<i data-lucide="Check" class="w-4 h-4 text-green-500" style="color: #10B981;"></i>'
+                                     activo_icono = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="Check" data-lucide="Check" class="lucide lucide-Check w-4 h-4 text-green-500" style="color: #10B981;"><polyline points="20 6 9 17 4 12"></polyline></svg>';
                                      //activo_icono = 'SI';
                                 }else{
-                                    activo_icono = '<i data-lucide="X" class="w-4 h-4 text-red-500" style="color: #EF4444;"></i>';
+                                    activo_icono = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="X" data-lucide="X" class="lucide lucide-X w-4 h-4 text-red-500" style="color: #EF4444;"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>';
                                     //activo_icono = 'NO';
                                 }
 
                                 if (row.casa_propia) {
-                                    casa_propia_icono = '<i data-lucide="Check" class="w-4 h-4 text-green-250" style="color: #10B981;"></i>'
+                                    casa_propia_icono = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="Check" data-lucide="Check" class="lucide lucide-Check w-4 h-4 text-green-500" style="color: #10B981;"><polyline points="20 6 9 17 4 12"></polyline></svg>';
                                     //casa_propia_icono = 'SI';
                                 }else{
-                                    casa_propia_icono = '<i data-lucide="X" class="w-4 h-4 text-red-250" style="color: #EF4444;"></i>';
+                                    casa_propia_icono = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="X" data-lucide="X" class="lucide lucide-X w-4 h-4 text-red-500" style="color: #EF4444;"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>';
                                     //casa_propia_icono = 'NO';
                                 }
 
